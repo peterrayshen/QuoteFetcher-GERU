cc96a70d74a807a95886fddd05e64c833c2b1018
Peter Shen <peterrayshen@gmail.com>
2017-10-31 20:50:06 -0400

    Deleted temporary test files

--- setup.py
+++ setup.py
@@ -9,14 +9,16 @@ requires = ['pyramid',
             ]
 
 setup(name='quotefetcher',
-      install_requires=requires,
-      entry_points={
-        'paste.app_factory': [
-            'main = quotefetcher:main',
-        ]},
-      author='Peter Ray Shen',
-      author_email='prshen@edu.uwaterloo.ca',
       description='''Simple web application that fetches quotes from an API, displays to user and stores requests/sessions 
       in a SQL database''',
+      install_requires=requires,
+      version='0.1',
+      url='https://github.com/peterrayshen/QuoteFetcher',
+      entry_points={
+          'paste.app_factory': [
+              'main = quotefetcher:main',
+          ]},
+      author='Peter Ray Shen',
+      author_email='prshen@edu.uwaterloo.ca',
 
-)
+      )
